dc6b30466c0092c97dde657ded7a7123af15e69f
Aaron Cimolini <aaron@cimolini.com>
2025-03-18 22:02:22 -0700

    OpenCV.js working with image threshold.

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-let imgElement = document.getElementById("imageSrc");
-let inputElement = document.getElementById("fileInput");
+var imgElement = document.getElementById("imageSrc");
+var inputElement = document.getElementById("fileInput");
 inputElement.addEventListener(
   "change",
   (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
   },
-  false
+  false,
 );
 
-imgElement.onload = function () {
-  let mat = cv.imread(imgElement);
-  cv.imshow("canvasOutput", mat);
-  mat.delete();
-};
+function onOpenCvReady() {
+  // eslint-disable-line no-unused-vars
+  document.getElementById("status").innerHTML =
+    "<b>OpenCV.js is ready</b>." +
+    "You can upload an image.<br>" +
+    "The <b>imageSrc</b> is a &lt;img&gt; element used as cv.Mat input. " +
+    "The <b>canvasOutput</b> is a &lt;canvas&gt; element used as cv.Mat output.";
 
-var Module = {
-  // https://emscripten.org/docs/api_reference/module.html#Module.onRuntimeInitialized
-  onRuntimeInitialized() {
-    document.getElementById("status").innerHTML = "OpenCV.js is ready.";
-  },
-};
+  cv.then((cv) => {
+    imgElement.onload = function () {
+      var src = cv.imread(imgElement);
+      var dst = new cv.Mat();
+      cv.threshold(src, dst, 177, 200, cv.THRESH_BINARY);
+      cv.imshow("canvasOutput", dst);
+      src.delete();
+      dst.delete();
+    };
+  });
+}
+
+function onOpenCvError() {
+  // eslint-disable-line no-unused-vars
+  var element = document.getElementById("status");
+  element.setAttribute("class", "err");
+  element.innerHTML = "Failed to load opencv.js";
+}
