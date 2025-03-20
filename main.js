ccb7e595d830cf3d4f7eb208a43be486e6dbb4b5
Aaron Cimolini <aaron@cimolini.com>
2025-03-19 18:30:16 -0700

    Roughs out basic object detection.

--- main.js
+++ main.js
@@ -5,8 +5,71 @@ inputElement.addEventListener(
   (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
   },
-  false,
+  false
 );
+
+function detectObjects(frame) {
+  const minArea = 1;
+  const maxArea = 9999;
+  const minAspectRatio = 0;
+  const maxAspectRatio = 0;
+
+  let gray = new cv.Mat();
+  let blurred = new cv.Mat();
+  let edges = new cv.Mat();
+  let contours = new cv.MatVector();
+  let hierarchy = new cv.Mat();
+
+  // 1. Preprocessing
+  cv.cvtColor(frame, gray, cv.COLOR_RGBA2GRAY);
+  cv.GaussianBlur(gray, blurred, new cv.Size(5, 5), 0); // Adjust size
+  cv.Canny(blurred, edges, 30, 90); // Adjust thresholds
+
+  // 2. Contour Detection
+  cv.findContours(
+    edges,
+    contours,
+    hierarchy,
+    cv.RETR_EXTERNAL,
+    cv.CHAIN_APPROX_SIMPLE
+  );
+
+  // 3. Shape Identification and Filtering
+  for (let i = 0; i < contours.size(); ++i) {
+    let contour = contours.get(i);
+    let peri = cv.arcLength(contour, true);
+    let approx = new cv.Mat();
+    cv.approxPolyDP(contour, approx, 0.04 * peri, true); // Adjust epsilon
+
+    if (approx.rows === 4) {
+      // Quadrilateral
+      let area = cv.contourArea(contour);
+      if (area > minArea && area < maxArea) {
+        // Area threshold
+        // Calculate aspect ratio (example)
+        let rect = cv.boundingRect(contour);
+        let aspectRatio = rect.width / rect.height;
+        console.log(aspectRatio);
+        if (aspectRatio > minAspectRatio && aspectRatio < maxAspectRatio) {
+          // ... Perspective Transform
+          //
+          // make checks to ensure we have an ID here.
+          //
+          // take photo of id for extracting data.
+        }
+      }
+    }
+    approx.delete();
+    contour.delete();
+  }
+
+  gray.delete();
+  blurred.delete();
+  contours.delete();
+  hierarchy.delete();
+
+  return edges;
+}
 
 function onOpenCvReady() {
   // eslint-disable-line no-unused-vars
@@ -18,12 +81,7 @@ function onOpenCvReady() {
 
   cv.then((cv) => {
     imgElement.onload = function () {
-      var src = cv.imread(imgElement);
-      var dst = new cv.Mat();
-      cv.threshold(src, dst, 177, 200, cv.THRESH_BINARY);
-      cv.imshow("canvasOutput", dst);
-      src.delete();
-      dst.delete();
+      cv.imshow("canvasOutput", detectObjects(cv.imread(imgElement)));
     };
   });
 }
