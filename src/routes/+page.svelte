572aed9ee849261f35f352b9d7e0a52980dc3cb3
Aaron Cimolini <aaron@cimolini.com>
2025-03-21 20:09:39 -0700

    Add video button.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,38 @@
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
+	import { fn } from '@storybook/test';
+
+	let isProcessingVideo = $state(false);
+	let paused = $state(true);
+	let videoSource = null;
+	let loadingVideo = false;
+
+	const toggleVideoStream = async () => {
+		try {
+			loadingVideo = true;
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: {
+					width: { ideal: 4096 },
+					height: { ideal: 2160 }
+				}
+			});
+			videoSource.srcObject = stream;
+			videoSource.play();
+			loadingVideo = false;
+		} catch (error) {
+			console.log(error);
+		}
+	};
 </script>
 
-<Card.Root class="w-full max-w-sm">
-	<Card.Header>
-		<Card.Title class="text-2xl">Login</Card.Title>
-		<Card.Description>Enter your email below to login to your account.</Card.Description>
-	</Card.Header>
-	<Card.Content class="grid gap-4">
-		<div class="grid gap-2">
-			<Label for="email">Email</Label>
-			<Input id="email" type="email" placeholder="m@example.com" required />
-		</div>
-		<div class="grid gap-2">
-			<Label for="password">Password</Label>
-			<Input id="password" type="password" required />
-		</div>
-	</Card.Content>
-	<Card.Footer>
-		<Button class="w-full">Sign in</Button>
-	</Card.Footer>
-</Card.Root>
+<div class="">
+	<Button class="" onclick={toggleVideoStream}>{isProcessingVideo ? 'Stop' : 'Start'} Video</Button>
+	{#if loadingVideo}
+		<h1>LOADING</h1>
+	{/if}
+	<video bind:this={videoSource} bind:paused>
+		<track kind="captions" />
+	</video>
+	<canvas id="canvasOutput"></canvas>
+</div>
